8c2834b3d6b0e4488cf9b9e2743219c119e9b3ce
kesvesna <kesvesna@rambler.ru>
2018-11-24 17:56:21 +0300

    bandwidth for 4 threads on my machine

---
+++ bandwidthForThreads.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <fstream>
+#include <time.h>
+#include <unistd.h>
+#include <vector>
+#include <mutex>
+
+using namespace std;
+using namespace std::chrono;
+
+long long warmUpOperations = 1000000;
+long long testSizeBuffer = 10000; //2147483647; // 2Gb
+long long iterations = 1000;
+long long threadsNumber = 1;
+long long maxThreadsNumber = thread::hardware_concurrency();
+bool timeResultsIsStored = false;
+mutex threadsMutex;
+ofstream fout;
+pthread_barrier_t open_barrier;
+
+int globalVariable = 4;
+int * ptrGlobalVariable = &globalVariable;
+int * buffer = (int *) malloc(testSizeBuffer);
+
+template <typename T>
+inline void functionCASAsm (const T * pAddr1, const T * oldValue1, const T * newValue1, const T * returnValue1)
+{
+	asm volatile ("lock\n\tcmpxchgq %1, %2" //cas assembler
+					: "=a" (returnValue1) : "r" (newValue1), "m" (pAddr1), "0" (oldValue1) : "memory"
+					);
+} 
+
+template <typename T2>
+inline void functionFAAAsm(const T2 * oldValue1, const T2 * pAddr1)
+{
+	asm volatile ("lock\n\tcmpxchgq %0, %1" // faa assembler
+				: "+r" (oldValue1), "+m" (pAddr1) : : "memory");
+}
+
+void warmUp()
+{
+	int forWarmUp = 5; // рандомное значение
+	int * pForWarmUp = &forWarmUp;
+	for ( long long i = 0; i < warmUpOperations; ++i )
+	{
+		functionCASAsm (pForWarmUp,&forWarmUp,&forWarmUp,&forWarmUp);
+	}
+}
+
+static __inline__ unsigned long long rdtsc(void) // для замера времени (абстрактного)
+{
+	unsigned hi, lo;
+	__asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+	return ((unsigned long long)lo) | (((unsigned long long )hi) << 32 );
+}
+
+void * testFunction(void * arg)
+{
+	long long operations = iterations;
+	unsigned long long int startRdtsc, endRdtsc;
+	vector <double> casTimeVector, faaTimeVector;	
+//======================================================================
+		pthread_barrier_wait(&open_barrier);
+		//warmUp();
+		startRdtsc = rdtsc();
+			for ( long long i = 0; i < operations; ++i )
+			{
+				functionCASAsm (ptrGlobalVariable,&globalVariable,&globalVariable,&globalVariable);
+			}
+		endRdtsc = rdtsc();
+		casTimeVector.push_back(operations/((double)(endRdtsc-startRdtsc)));
+//======================================================================
+		pthread_barrier_wait(&open_barrier);
+		//warmUp();
+		startRdtsc = rdtsc();
+			for ( long long i = 0; i < operations; ++i)
+			{
+				functionFAAAsm (&globalVariable,ptrGlobalVariable);
+			}
+		endRdtsc = rdtsc();
+		faaTimeVector.push_back(operations/((double)(endRdtsc-startRdtsc)));
+//======================================================================
+	threadsMutex.lock(); // для поочередной записи в файл, чтобы потоки не портили данные
+	double tempCasTime = 0;
+    for ( double n : casTimeVector ) // запись результатов замеров CAS от всех потоков
+	{
+		//fout << "arrCasAsmTime = [arrCasAsmTime " << n << " ];" << endl;
+		tempCasTime += n;
+    }
+    double tempFaaTime = 0;
+    for ( double n : faaTimeVector ) // запись результатов замеров FAA от всех потоков
+	{
+		//fout << "arrFaaAsmTime = [arrFaaAsmTime " << n << "];" << endl;
+		tempFaaTime += n;
+    }
+    threadsMutex.unlock();
+    pthread_barrier_wait(&open_barrier); // гарантировано, 
+    //что все потоки сделали запись времени в переменную temp
+    threadsMutex.lock();
+    if (!timeResultsIsStored) // запись среднего времени в файл только одним потоком
+    {
+		tempCasTime /= threadsNumber;
+		tempFaaTime /= threadsNumber;
+		fout << "arrDataSize = [arrDataSize " << threadsNumber << "];" << endl;
+		fout << "arrCasAsmTime = [arrCasAsmTime " << tempCasTime << "];" << endl;
+		fout << "arrFaaAsmTime = [arrFaaAsmTime " << tempFaaTime << "];" << endl;
+		timeResultsIsStored = true;
+	}
+    threadsMutex.unlock();
+	return 0;
+}
+
+int main ()
+{
+	if (buffer == NULL) 
+	{
+		cout << "Не удалось выделить память" << endl;
+		exit (1);
+	}
+	for (int ix = 0; ix < testSizeBuffer; ix++)
+	{
+		//buffer[ix] = rand() % 26 + 'a'; // раскомментить после отладки
+		buffer[ix] = 3; // для отладки, быстрое заполнение буфера, закомментить после отладки
+	}
+	fout.open("resultWithDataBuffer.m");
+	time_t myTime;
+	myTime = time(NULL);
+	cout << "Test start: " << ctime(&myTime) << endl;
+	fout << "%Test start: " << ctime(&myTime) << endl;
+	long mempagesize = sysconf(_SC_PAGESIZE);
+	cout << "Memory pagesize on this machine : " << mempagesize << " bytes" << endl << endl;
+	cout << "Max threads number: " << maxThreadsNumber << endl;
+	fout << "%Memory pagesize on this machine : " << mempagesize << " bytes" << endl << endl;
+	fout << "arrDataSize = [];" << endl;
+	fout << "arrCasAsmTime = [];" << endl;
+	fout << "arrFaaAsmTime = [];" << endl;
+	do { 
+	pthread_t threads[threadsNumber];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_barrier_init(&open_barrier, NULL, threadsNumber);
+	cpu_set_t mask;
+    for (int i = 0; i < threadsNumber; ++i) 
+    {	 
+		CPU_ZERO(&mask);
+		CPU_SET(i, &mask);
+		pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &mask);
+		sched_setaffinity(0,sizeof(cpu_set_t),&mask);
+		pthread_create(&threads[i], &attr, testFunction, NULL);
+    }
+    for (int i = 0; i < threadsNumber; ++i) 
+    {
+        pthread_join(threads[i],NULL);
+    }
+    threadsNumber ++;
+    timeResultsIsStored = false;
+	} while ( threadsNumber <= maxThreadsNumber );// от одного до max возможного числа потоков
+	time_t myTime2;
+	myTime2 = time(NULL);
+	cout << endl << "Test finish: " << ctime(&myTime2) << endl;
+	fout << "%Test finish: " << ctime(&myTime2) << endl;
+	fout << "hold on" << endl;
+	//fout << "axis([-1000 70000 -2 2])" << endl;
+	fout << "plot(arrDataSize,arrCasAsmTime,'*-')" << endl;
+	fout << "plot(arrDataSize,arrFaaAsmTime,'o-')" << endl;
+	fout << "grid on" << endl;
+	fout << "legend('Cas','Faa','North')" << endl;
+    fout << "ylabel('bandwidth [bytes/cycle]')" << endl;
+    fout << "xlabel('threads numbers [pieces]')" << endl;
+	fout.close();
+	return 0;
+}
