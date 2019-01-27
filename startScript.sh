698920e46955bfcd148c5c1770073c67538f4f05
kesvesna <kesvesna@rambler.ru>
2019-01-27 12:54:57 +0300

    New version

--- startScript.sh
+++ startScript.sh
@@ -1,7 +1,11 @@
-sudo sysctl -w vm.nr_hugepages=1000
-echo "Количество страниц"
-sudo cat /proc/sys/vm/nr_hugepages
+# enable and tune huge page
+# hugeadm --pool-list  / to see pool list, min, max sizes and default size
+# hugeadm --pool-pages-min 2MB:512  /To set the 2MB pool minimum to 512 pages
+# hugeadm --pool-pages-max 2MB:2048  /And to set our 2048 max pages
+# hugeadm --explain  All about 
 ./atomicOperations
+
+
 
 
 
