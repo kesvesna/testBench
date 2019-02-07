cbbe968ccb5dfd0a90f9f420f7030e57df270bb3
kesvesna <kesvesna@rambler.ru>
2019-02-07 19:58:11 +0300

    New version

---
+++ startScript.sh
@@ -0,0 +1,11 @@
+# enable and tune huge page
+# hugeadm --pool-list  / to see pool list, min, max sizes and default size
+# hugeadm --pool-pages-min 2MB:512  /To set the 2MB pool minimum to 512 pages
+# hugeadm --pool-pages-max 2MB:2048  /And to set our 2048 max pages
+# hugeadm --explain  All about 
+./atomicOperations
+
+
+
+
+
